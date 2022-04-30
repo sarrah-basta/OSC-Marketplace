a87668b5ebbfd4329e575df3220488c9ab9c272c
sarrah-basta <sarrah.basta@gmail.com>
2022-04-30 16:08:18 +0530

    Updated config settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,23 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
 const projectId = "a972b71cb9e54ec7bb8f4e703e9ee6cf"
 
 
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    mumbai: {
-      url:`https://polygon-mumbai.infura.io/v3/${projectId}`,
-    },
-    mainnet: {
-      url:`https://polygon-mainnet.infura.io/v3/${projectId}`,
-    },
-  
+    //currently only running on localhost
   },
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
